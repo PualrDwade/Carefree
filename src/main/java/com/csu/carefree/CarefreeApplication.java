2e0b5799aa29b041677fb9894c9cf758ab6b1ce4
志轩 陈 <544493924@qq.com>
2018-09-04 22:59:09 +0800

    version1.3

--- src/main/java/com/csu/carefree/CarefreeApplication.java
+++ src/main/java/com/csu/carefree/CarefreeApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// springboot启动项目
 public class CarefreeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CarefreeApplication.class, args);
     }
