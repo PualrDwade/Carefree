74c4bcb11790c0596c0c1ce23e38592df7065cc7
志轩 陈 <544493924@qq.com>
2018-09-15 02:07:48 +0800

    添加了个人中心模块的接口

--- src/main/java/com/csu/carefree/Service/AccountService.java
+++ src/main/java/com/csu/carefree/Service/AccountService.java
@@ -4,10 +4,17 @@ package com.csu.carefree.Service;
 import com.csu.carefree.Model.Account.EmailVerifyRecord;
 import com.csu.carefree.Model.Account.Sigon;
 import com.csu.carefree.Model.Account.UserProfile;
+import com.csu.carefree.Model.TraverAsk.TraverNote;
+import com.csu.carefree.Model.TraverAsk.UserAnswer;
+import com.csu.carefree.Model.TraverAsk.UserAsk;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface AccountService {
+
+    /****************用户登陆注册模块Service****************************************/
     //通过用户名得到登陆账号信息
     Sigon getSigon(String username);
 
@@ -17,6 +24,15 @@ public interface AccountService {
     //通过邮箱找回密码
     boolean updateSigon(String username, String password);
 
+    //通过邮箱得到验证码
+    String getVerifyCode(String username, String type);
+
+    //插入邮箱验证码信息
+    boolean setVerifyCode(String username, String code, String type);
+    /*****************************************************************************/
+
+
+    /**********************用户中心模块Service*************************************/
     //通过userprofile创建用户详细信息
     boolean setUserProfile(UserProfile userProfile);
 
@@ -26,10 +42,18 @@ public interface AccountService {
     //通过username得到用户详细信息
     UserProfile getUserProfile(String username);
 
-    //通过邮箱得到验证码
-    String getVerifyCode(String username, String type);
+    //通过username得到用户游记列表
+    List<TraverNote> getTraverNodeListbyName(String username);
 
+    //通过username得到用户问题列表
+    List<UserAsk> getUserAskListbyName(String username);
 
-    //插入邮箱验证码信息
-    boolean setVerifyCode(String username, String code, String type);
+    //通过username得到用户回答列表
+    List<UserAnswer> getUserAnswerListByName(String username);
+
+    //通过用户问题ID获取问题答案
+    List<UserAnswer> getUserAnswerByAsk(String userAskId);
+
+    /*****************************************************************************/
+
 }
