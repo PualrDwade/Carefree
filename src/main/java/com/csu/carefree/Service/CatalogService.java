1be5ef6c2ed08d28af1247a9940576a56774ce2f
hshen9527 <2391255979@qq.com>
2018-09-15 00:02:10 +0800

    加入其他Mapper

--- src/main/java/com/csu/carefree/Service/CatalogService.java
+++ src/main/java/com/csu/carefree/Service/CatalogService.java
@@ -1,9 +1,8 @@
 package com.csu.carefree.Service;
 
 
-import com.csu.carefree.Model.ProductDT.HotelMsg;
-import com.csu.carefree.Model.ProductDT.ProductMsg;
-import com.csu.carefree.Model.ProductDT.Supplier;
+import com.csu.carefree.Model.ProductDT.*;
+import com.csu.carefree.Model.TraverAsk.TraverNote;
 import com.csu.carefree.Model.TraverMsg.CityMsg;
 import com.csu.carefree.Model.TraverMsg.ProvinceMsg;
 import com.csu.carefree.Model.TraverMsg.ScenicMsg;
@@ -13,6 +12,8 @@ import java.util.List;
 
 @Service
 public interface CatalogService {
+
+    /*******************酒店信息**********/
     //通过销量获取热门酒店列表
     List<HotelMsg> getHotelList();
 
@@ -80,4 +81,50 @@ public interface CatalogService {
 
     //通过景点名称获取景点信息
     List<ScenicMsg> getScenicMsgListByName(String name);
+
+    /*************产品城市信息*****/
+    //获得产品的所有有关城市信息
+    List<Product_CityMsg> getProduct_CityList();
+
+    //根据产品ID找出产品的所有出发城市以及相应的起价
+    List<Product_CityMsg> getAllDepartCityListByProductId(String product_id);
+
+    //根据产品ID以及出发城市名字查找相应的起价
+    Product_CityMsg getADepartCity_Price(String Product_id, String cityName);
+
+    /*****************产品景点信息******/
+    //根据景点名字查找产品列表
+    List<Product_ScenicMsg> getProductIdsByScenicName(String scenicName);
+
+    //根据产品ID查找景点名字列表
+    List<Product_ScenicMsg> getScenicNamesByProductId(String ProductId);
+
+    /***************攻略信息****/
+    // 得到攻略信息
+    List<StrategyMsg> getStrategyList();
+
+    // 根据景点得到
+    StrategyMsg getStrategyByScenic_name(String scenic_name);
+
+    /*****************门票信息0******/
+    //得到所有门票信息
+    List<TicketMsg> getTicketList();
+
+    //门票查询筛选信息有：景点名字、城市名字
+    List<TicketMsg> getTicketListByScenic_name(String scenic_name);
+
+    List<TicketMsg> getTicketListByCity_id(String city_id);
+
+    /**************游记信息****/
+    //获取所有游记信息
+    List<TraverNote> getTraverNoteList();
+
+    //通过关键词搜索游记信息
+    List<TraverNote> searchTraverNoteList(String keyword);
+
+    //通过游记ID获取游记信息
+    TraverNote getTraverNoteById(String traverNoteId);
+
+    //存储用户游记信息
+    void insertTraverNote(TraverNote traverNote);
 }
