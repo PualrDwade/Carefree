6856409a45fe53181b3a798626e28aab294ec30c
志轩 陈 <544493924@qq.com>
2018-09-15 15:11:51 +0800

    修改了服务层接口的命名以及实现类

--- src/main/java/com/csu/carefree/Service/CatalogService.java
+++ src/main/java/com/csu/carefree/Service/CatalogService.java
@@ -14,52 +14,54 @@ import java.util.List;
 public interface CatalogService {
 
     /*******************酒店信息**********/
-    //通过销量获取热门酒店列表
-    List<HotelMsg> getHotelList();
+    //获取所有酒店信息
+    List<HotelMsg> getHotelMsgList();
 
-    //获取酒店具体信息
-    HotelMsg getHotelMsg(String hotelId);
+    //通过酒店ID获取酒店具体信息
+    HotelMsg getHotelMsg(String id);
 
-    //通过用户输入的目的地获取酒店信息
+    //通过关键字搜索酒店
+    List<HotelMsg> searchHotelMsgList(String keyword);
+
+    //根据目的地获得酒店信息
     List<HotelMsg> getHotelListByDestination(String destination);
 
-    //通过关键词搜索获取酒店信息
-    List<HotelMsg> getHotelListByKeyword(String keyword);
+
+    /*****************通过筛选条件获取产品*****************/
 
     List<ProductMsg> getProductList();
 
     ProductMsg getProductById(String id);
 
-    /*****************通过筛选条件获取产品*****************/
     //筛选条件：traver_days, product_type, supplier_id
 
     //行程天数
-    List<ProductMsg> getProductListByTraver_days(String traver_days);
+    List<ProductMsg> getProductListByTraverdays(String traverDays);
 
     //产品类型
-    List<ProductMsg> getProductListByProduct_type(String product_type);
+    List<ProductMsg> getProductListByProductType(String productType);
 
     //供应商
-    List<ProductMsg> getProductListBySupplier_id(String supplier_id);
+    List<ProductMsg> getProductListBySupplierId(String supplierId);
 
     //产品名称
-    List<ProductMsg> getProductListByName(String product_name);
+    List<ProductMsg> getProductListByName(String productName);
 
     //三个筛选条件都有
-    List<ProductMsg> getProductListByThree(String traver_days, String product_type, String supplier_id);
+    List<ProductMsg> getProductListByThree(String traverDays, String productType, String supplierId);
 
     //*****************通过推荐条件来排序、推荐*****************/
     //注意：推荐条件是在筛选条件的基础上进行的，所以不能直接对数据库进行查找
     //推荐（排序）条件：score, sell_num, comments_num, product_grade
 
-    /*******供应商信息*****/
+    /*******供应商信息*********************************/
     //获取供应商列表
     List<Supplier> getSupplierList();
 
     //例如当点击产品当中的供应商信息时候，要返回相应的信息
     Supplier getSupplierByName(String name);
 
-    /*******城市信息*******/
+    /*******城市省份信息*******/
     //通过省份ID获取省份信息
     ProvinceMsg getProvinceMsgById(String provinceId);
 
@@ -70,10 +72,11 @@ public interface CatalogService {
     List<CityMsg> searchCityMsgByName(String cityName);
 
     //通过城市ID获取城市所有景点信息
-    List<ScenicMsg> getScenicMsgListById(String cityId);
+    List<ScenicMsg> getScenicMsgListByCityId(String cityId);
 
     //通过城市名字获取城市所有景点信息
     List<ScenicMsg> getScenicMsgListByCityName(String cityName);
+
 
     /***********景点信息*********/
     //通过景点ID获取景点信息
@@ -84,36 +87,36 @@ public interface CatalogService {
 
     /*************产品城市信息*****/
     //获得产品的所有有关城市信息
-    List<Product_CityMsg> getProduct_CityList();
+    List<ProductCityMsg> getProductCityList();
 
     //根据产品ID找出产品的所有出发城市以及相应的起价
-    List<Product_CityMsg> getAllDepartCityListByProductId(String product_id);
+    List<ProductCityMsg> getAllDepartCityListByProductId(String productId);
 
     //根据产品ID以及出发城市名字查找相应的起价
-    Product_CityMsg getADepartCity_Price(String Product_id, String cityName);
+    ProductCityMsg getDepartCityPrice(String productId, String cityName);
 
     /*****************产品景点信息******/
     //根据景点名字查找产品列表
-    List<Product_ScenicMsg> getProductIdsByScenicName(String scenicName);
+    List<ProductScenicMsg> getProductIdsByScenicName(String scenicName);
 
     //根据产品ID查找景点名字列表
-    List<Product_ScenicMsg> getScenicNamesByProductId(String ProductId);
+    List<ProductScenicMsg> getScenicNamesByProductId(String productId);
 
     /***************攻略信息****/
     // 得到攻略信息
     List<StrategyMsg> getStrategyList();
 
     // 根据景点得到
-    StrategyMsg getStrategyByScenic_name(String scenic_name);
+    StrategyMsg getStrategyByScenicName(String scenicName);
 
-    /*****************门票信息0******/
+    /*****************门票信息******/
     //得到所有门票信息
     List<TicketMsg> getTicketList();
 
     //门票查询筛选信息有：景点名字、城市名字
-    List<TicketMsg> getTicketListByScenic_name(String scenic_name);
+    List<TicketMsg> getTicketListByScenicName(String scenicName);
 
-    List<TicketMsg> getTicketListByCity_id(String city_id);
+    List<TicketMsg> getTicketListByCityId(String cityId);
 
     /**************游记信息*******/
     //获取所有游记信息
@@ -124,7 +127,4 @@ public interface CatalogService {
 
     //通过游记ID获取游记信息
     TraverNote getTraverNoteById(String traverNoteId);
-
-    //存储用户游记信息
-    void insertTraverNote(TraverNote traverNote);
 }
--- src/main/java/com/csu/carefree/Service/impl/CatalogServiceImpl.java
+++ src/main/java/com/csu/carefree/Service/impl/CatalogServiceImpl.java
@@ -8,7 +8,6 @@ import com.csu.carefree.Model.TraverMsg.ScenicMsg;
 import com.csu.carefree.Persistence.*;
 import com.csu.carefree.Service.CatalogService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -32,10 +31,10 @@ public class CatalogServiceImpl implements CatalogService {
     private SupplierMapper supplierMapper;
 
     @Autowired
-    private Product_CityMsgMapper product_cityMsgMapper;
+    private ProductCityMsgMapper productCityMsgMapper;
 
     @Autowired
-    private Product_ScenicMsgMapper product_scenicMsgMapper;
+    private ProductScenicMsgMapper productscenicMsgMapper;
 
     @Autowired
     private ProvinceMsgMapper provinceMsgMapper;
@@ -48,214 +47,172 @@ public class CatalogServiceImpl implements CatalogService {
 
     @Autowired
     private TraverNoteMapper traverNoteMapper;
-  
-  
-    /*******酒店信息******/
-    @Override//通过销量获取热门酒店列表
-    public List<HotelMsg> getHotelList() {
-        return hotelMsgMapper.getHotelMsgList();
+
+    @Override
+    public List<HotelMsg> getHotelMsgList() {
+        return null;
     }
 
-
-    @Override//获取酒店具体信息
-    public HotelMsg getHotelMsg(String hotelId) {
-        return hotelMsgMapper.getHotelMsg(hotelId);
+    @Override
+    public HotelMsg getHotelMsg(String id) {
+        return hotelMsgMapper.getHotelMsg(id);
     }
 
-
-    @Override//通过关键词搜索获取酒店信息
-    public List<HotelMsg> getHotelListByKeyword(String keyword) {
-        return hotelMsgMapper.searchHotelMsgList("%" + keyword + "%");
+    @Override
+    public List<HotelMsg> searchHotelMsgList(String keyword) {
+        return searchHotelMsgList(keyword);
     }
 
     @Override
     public List<HotelMsg> getHotelListByDestination(String destination) {
         return hotelMsgMapper.getHotelListByDestination(destination);
     }
-    /********************************************************************************/
 
-
-    /******产品信息******/
     @Override
-    public List<ProductMsg> getProductList() {      //获取所有产品列表-倒序
+    public List<ProductMsg> getProductList() {
         return productMapper.getProductList();
     }
 
     @Override
-    public ProductMsg getProductById(String id) {    //通过产品ID获取产品信息
+    public ProductMsg getProductById(String id) {
         return productMapper.getProductById(id);
     }
 
-    /*****************通过筛选条件获取产品****************/
     @Override
-    public List<ProductMsg> getProductListByTraver_days(String traver_days) {    //行程天数
-        return productMapper.getProductListByTraver_days(traver_days);
+    public List<ProductMsg> getProductListByTraverdays(String traverDays) {
+        return productMapper.getProductListByTraverdays(traverDays);
     }
 
     @Override
-    public List<ProductMsg> getProductListByProduct_type(String product_type) {    //产品类型
-        return productMapper.getProductListByProduct_type(product_type);
+    public List<ProductMsg> getProductListByProductType(String productType) {
+        return productMapper.getProductListByProductType(productType);
     }
 
     @Override
-    public List<ProductMsg> getProductListBySupplier_id(String supplier_id) {    //供应商
-        return productMapper.getProductListBySupplier_id(supplier_id);
+    public List<ProductMsg> getProductListBySupplierId(String supplierId) {
+        return productMapper.getProductListBySupplierId(supplierId);
     }
 
     @Override
-    public List<ProductMsg> getProductListByName(String product_name) {    //产品名称
-        return productMapper.getProductListByName("%" + product_name + "%");
+    public List<ProductMsg> getProductListByName(String productName) {
+        return productMapper.getProductListByName(productName);
     }
 
     @Override
-    public List<ProductMsg> getProductListByThree(String traver_days, String product_type, String supplier_id) {
-        //三个筛选条件都有
-        return productMapper.getProductListByThree(traver_days, product_type, supplier_id);
+    public List<ProductMsg> getProductListByThree(String traverDays, String productType, String supplierId) {
+        return productMapper.getProductListByThree(traverDays, productType, supplierId);
     }
 
-    //*****************通过推荐条件来排序、推荐*****************/
-    //注意：推荐条件是在筛选条件的基础上进行的，所以不能直接对数据库进行查找
-    //推荐（排序）条件：score, sell_num, comments_num, product_grade
-
-
-    /*******供应商信息*****/
     @Override
-    public List<Supplier> getSupplierList() {    //获取供应商列表
+    public List<Supplier> getSupplierList() {
         return supplierMapper.getSupplierList();
     }
 
     @Override
-    public Supplier getSupplierByName(String name) {    //例如当点击产品当中的供应商信息时候，要返回相应的信息
+    public Supplier getSupplierByName(String name) {
         return supplierMapper.getSupplierByName(name);
     }
 
-    /*******城市信息*******/
     @Override
-    public ProvinceMsg getProvinceMsgById(String provinceId) {    //通过省份ID获取省份信息
-        return cityMsgMapper.getProvinceMsgById(provinceId);
+    public ProvinceMsg getProvinceMsgById(String provinceId) {
+        return provinceMsgMapper.getProvinceMsgById(provinceId);
     }
 
     @Override
-
-    public CityMsg getCityMsgById(String cityId) {    //通过城市ID获取城市信息
+    public CityMsg getCityMsgById(String cityId) {
         return cityMsgMapper.getCityMsgById(cityId);
     }
 
     @Override
-    public List<CityMsg> searchCityMsgByName(String cityName){    //通过城市名称获取城市信息(模糊搜索)
-        return cityMsgMapper.searchCityMsgByName("%"+cityName+"%");
-    }
-  
-    @Override
-    public List<ScenicMsg> getScenicMsgListById(String cityId) {    //通过城市ID获取城市所有景点信息
-        return cityMsgMapper.getScenicMsgListById(cityId);
+    public List<CityMsg> searchCityMsgByName(String cityName) {
+        return cityMsgMapper.searchCityMsgByName(cityName);
     }
 
     @Override
-    public List<ScenicMsg> getScenicMsgListByCityName(String cityName) {    //通过城市名字获取城市所有景点信息
-        return cityMsgMapper.getScenicMsgListByCityName(cityName);
+    public List<ScenicMsg> getScenicMsgListByCityId(String cityId) {
+        return scenicMsgMapper.getScenicMsgListByCityId(cityId);
     }
 
-    /***********景点信息*********/
-    @Override
 
-    public ScenicMsg getScenicMsgById(String scenicId) {    //通过景点ID获取景点信息
+    @Override
+    public List<ScenicMsg> getScenicMsgListByCityName(String cityName) {
+        return scenicMsgMapper.getScenicMsgListByCityName(cityName);
+    }
+
+    @Override
+    public ScenicMsg getScenicMsgById(String scenicId) {
         return scenicMsgMapper.getScenicMsgById(scenicId);
     }
 
     @Override
-    public List<ScenicMsg> getScenicMsgListByName(String name) {    //通过景点名称获取景点信息
-        return scenicMsgMapper.getScenicMsgListByName("%" + name + "%");
+    public List<ScenicMsg> getScenicMsgListByName(String name) {
+        return scenicMsgMapper.getScenicMsgListByName(name);
     }
 
-    /*************产品城市信息*****/
     @Override
-    public List<Product_CityMsg> getProduct_CityList(){    //获得产品的所有有关城市信息
-        return product_cityMsgMapper.getProduct_CityList();
+    public List<ProductCityMsg> getProductCityList() {
+        return productCityMsgMapper.getProductCityList();
     }
 
-    @Override//根据产品ID找出产品的所有出发城市以及相应的起价
-    public List<Product_CityMsg> getAllDepartCityListByProductId(String product_id){
-        return product_cityMsgMapper.getAllDepartCityListByProductId(product_id);
+    @Override
+    public List<ProductCityMsg> getAllDepartCityListByProductId(String productId) {
+        return productCityMsgMapper.getAllDepartCityListByProductId(productId);
     }
 
-    @Override//根据产品ID以及出发城市名字查找相应的起价
-    public Product_CityMsg getADepartCity_Price(String Product_id, String cityName){
-        return product_cityMsgMapper.getADepartCity_Price(Product_id,cityName);
+    @Override
+    public ProductCityMsg getDepartCityPrice(String productId, String cityName) {
+        return productCityMsgMapper.getDepartCityPriceByProductId(productId, cityName);
     }
 
-    /*****************产品景点信息******/
-    @Override//根据景点名字查找产品列表
-    public List<Product_ScenicMsg> getProductIdsByScenicName(String scenicName){
-        return product_scenicMsgMapper.getProductIdsByScenicName(scenicName);
+    @Override
+    public List<ProductScenicMsg> getProductIdsByScenicName(String scenicName) {
+        return productscenicMsgMapper.getProductIdsByScenicName(scenicName);
     }
 
-    @Override//根据产品ID查找景点名字列表
-    public List<Product_ScenicMsg> getScenicNamesByProductId(String ProductId){
-        return product_scenicMsgMapper.getScenicNamesByProductId(ProductId);
+    @Override
+    public List<ProductScenicMsg> getScenicNamesByProductId(String productId) {
+        return productscenicMsgMapper.getScenicNamesByProductId(productId);
     }
 
-    /*****************省份信息*********/
-    @Override//根据省份ID获取城市信息
-    public ProvinceMsg getProvinceMsgById(String provinceId){
-        return provinceMsgMapper.getProvinceMsgById(provinceId);
-    }
-
-    @Override//根据省份名称名称获取城市信息
-    public List<ProvinceMsg> searchProvinceMsgByName(String provinceName){
-        return provinceMsgMapper.searchProvinceMsgByName("%"+provinceName+"%");
-    }
-
-    @Override//根据省份ID获取省份所有城市信息
-    public List<CityMsg> getCityListById(String provinceId){
-        return provinceMsgMapper.getCityListById(provinceId);
-    }
-
-    /***************攻略信息****/
-    @Override// 得到攻略信息
-    public List<StrategyMsg> getStrategyList(){
+    @Override
+    public List<StrategyMsg> getStrategyList() {
         return strategyMsgMapper.getStrategyList();
     }
 
-    @Override// 根据景点得到
-    public StrategyMsg getStrategyByScenic_name(String scenic_name){
-        return strategyMsgMapper.getStrategyByScenic_name(scenic_name);
+    @Override
+    public StrategyMsg getStrategyByScenicName(String scenicName) {
+        return strategyMsgMapper.getStrategyByScenicName(scenicName);
     }
 
-    /*****************门票信息0******/
-    @Override//得到所有门票信息
-    public List<TicketMsg> getTicketList(){
+
+    @Override
+    public List<TicketMsg> getTicketList() {
         return ticketMsgMapper.getTicketList();
     }
 
-    @Override//门票查询筛选信息有：景点名字、城市名字
-    public List<TicketMsg> getTicketListByScenic_name(String scenic_name){
-        return ticketMsgMapper.getTicketListByScenic_name(scenic_name);
+    @Override
+    public List<TicketMsg> getTicketListByScenicName(String scenicName) {
+        return ticketMsgMapper.getTicketListByScenicName(scenicName);
     }
 
     @Override
-    public List<TicketMsg> getTicketListByCity_id(String city_id){
-        return ticketMsgMapper.getTicketListByCity_id(city_id);
+    public List<TicketMsg> getTicketListByCityId(String cityId) {
+        return ticketMsgMapper.getTicketListByCityId(cityId);
     }
 
-    /**************游记信息****/
-    @Override//获取所有游记信息
-    public List<TraverNote> getTraverNoteList(){
+
+    @Override
+    public List<TraverNote> getTraverNoteList() {
         return traverNoteMapper.getTraverNoteList();
     }
 
-    @Override//通过关键词搜索游记信息
-    public List<TraverNote> searchTraverNoteList(String keyword){
-        return traverNoteMapper.searchTraverNoteList("%"+keyword+"%");
+    @Override
+    public List<TraverNote> searchTraverNoteList(String keyword) {
+        return traverNoteMapper.searchTraverNoteList(keyword);
     }
 
-    @Override//通过游记ID获取游记信息
-    public TraverNote getTraverNoteById(String traverNoteId){
+    @Override
+    public TraverNote getTraverNoteById(String traverNoteId) {
         return traverNoteMapper.getTraverNoteById(traverNoteId);
-    }
-
-    @Override//存储用户游记信息
-    public void insertTraverNote(TraverNote traverNote){
-        traverNoteMapper.insertTraverNote(traverNote);
     }
 }
